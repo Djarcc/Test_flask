5d8607e2bfa93a78623d556659681c17776fe7af
djarc <dj.arvore@gmail.com>
2024-03-01 18:27:54 -0400

    second

---
+++ database/cliente.py
@@ -0,0 +1,8 @@
+CLIENTES = [
+    {"id":1, "nome": "Guilherme", "email": "test@test.com"},
+    {"id":2, "nome": "Jorge", "email": "tess@test.com"},
+    {"id":3, "nome": "Maria", "email": "testt@test.com"},
+    {"id":4, "nome": "Felipe", "email": "testtt@test.com"},
+    {"id":5, "nome": "joao", "email": "tesss@test.com"},
+    {"id":6, "nome": "jesus", "email": "tessss@test.com"}
+  ]
